465158f9d7ca02d5b80bc8196e8cfa89fec22c57
elad <eladlevavi@gmail.com>
2021-03-29 19:43:38 +0300

    Fix index and contact pages design

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 setInterval(changePic, 3000);
 
-var picArr = ["images/250X110668568400.jpg", "images/paz_250x110059890678.jpg", "images/paz_250x110103131473.jpg"];
+var picArr = ["images/250X110668568400.jpg", "images/paz_250x110059890678.jpg", "images/banner_atar.jpg"];
 var i = 0;
 
 function changePic() {
